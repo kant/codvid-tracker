e0a91f6ab60686b2c99d423f5c5807d3f035ced6
Dani-Linux <dani.linux.97@gmail.com>
2020-06-28 15:17:35 +0200

    Added new Font

--- src/App.vue
+++ src/App.vue
@@ -22,10 +22,12 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap");
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: "Fjalla One", sans-serif;
 }
 #app {
   width: 100%;
