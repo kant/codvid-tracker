19274c7945f6ae985d5b455b0207e860507144d4
Dani-Linux <dani.linux.97@gmail.com>
2020-06-28 15:53:13 +0200

    Fixed a lil bug

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -5,24 +5,25 @@
       <p>This is a Corona Virus (Codvid-19) Tracker</p>
       <p>
         Developed by
-        <a href=" https://twitter.com/Dani60579343" class="link" target="_blank"
-          >Korak Kurani (Dani)</a
-        >
+        <a
+          href=" https://twitter.com/Dani60579343"
+          class="link"
+          target="_blank"
+        >Korak Kurani (Dani)</a>
       </p>
       <br />
       <h3>Some info about the App :</h3>
       <p>The application is still under Development.</p>
       <p>We are Sorry if there will be any Issues</p>
       <h4>How the App works ?</h4>
-      <p>
-        When the Application starts it asks for Permmision to use your Location.
-      </p>
+      <p>When the Application starts it asks for Permmision to use your Location.</p>
       <p>
         Please Provide us that Permission so we will be able to show your
         Current Location.
       </p>
       <p>
-        As default, By clicking <strong>Latest</strong> on the Worldwide or
+        As default, By clicking
+        <strong>Latest</strong> on the Worldwide or
         Current Location. You will be able to get Statics (Number of Deathes and
         Confrimed cases).
       </p>
@@ -32,26 +33,23 @@
       </p>
       <p>Currently we are only able to provide Statics based on Countries.</p>
       <p>
-        We also can only provide <strong>Number of Death</strong> and
+        We also can only provide
+        <strong>Number of Death</strong> and
         <strong>Number of confirmed cases</strong>
       </p>
       <br />
       <h3>NOTE:</h3>
-      <p>
-        The searches are based on the Country names or their ISO-2 digit code.
-      </p>
-      <p>
-        In order to face no problems please write the correct Search values.
-      </p>
+      <p>The searches are based on the Country names or their ISO-2 digit code.</p>
+      <p>In order to face no problems please write the correct Search values.</p>
       <p>Country names like : Germany</p>
       <p>ISO-2 digit code like : DE</p>
-      <router-link v-bind:to="'/about'" class="router-link"
-        ><p class="link">Click Here to Read More</p></router-link
-      >
+      <router-link v-bind:to="'/about'" class="router-link">
+        <p class="link">Click Here to Read More</p>
+      </router-link>
     </div>
-    <router-link v-bind:to="'/main'" class="router-link"
-      ><p class="link">CONTINUE</p></router-link
-    >
+    <router-link v-bind:to="'/main'" class="router-link">
+      <p class="link">CONTINUE</p>
+    </router-link>
   </div>
 </template>
 
@@ -59,7 +57,7 @@
 export default {
   data() {
     return {};
-  },
+  }
 };
 </script>
 
@@ -77,6 +75,9 @@ export default {
   line-height: 30px;
   transition: ease 1s;
 }
+h1 {
+  margin: 30px auto;
+}
 .link {
   color: snow;
   padding: 10px;
@@ -90,5 +91,10 @@ export default {
 }
 .router-link {
   text-decoration: none;
+}
+@media (max-width: 490px) {
+  * {
+    font-size: 18px;
+  }
 }
 </style>
